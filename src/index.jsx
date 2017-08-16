31cafc2787c46fb8589fb37c63e0075fde17fc61
饭鱼 <fanyu.xhy@taobao.com>
2017-08-16 17:51:55 +0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
 import AppState from './AppState';
 import App from './App';
 
 const appState = new AppState();
 
-render(
-  <AppContainer>
-    <App appState={appState} />
-  </AppContainer>,
-  document.getElementById('root')
-);
+render(<App appState={appState} />, document.getElementById('root'));
